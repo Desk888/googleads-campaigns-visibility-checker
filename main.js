d4db50b82a22dd60ec74a850687898370170a4d1
Lorenzo Filippini <104449879+Desk888@users.noreply.github.com>
2024-06-17 10:03:05 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
 // Configurations
 let config = {
-    impressionThreshold: 10, // Change Impr. Threshold
-    emailAddresses: "Enter email address here", // Separate email addresses with a comma
+    impressionThreshold: 5, // Change Impr. Threshold
+    emailAddresses: "lorenzo.filippini@croud.com", // Separate email addresses with a comma
     emailSubject: "Daily Campaign Visibility Checks",
-    emailMessage: "The following campaigns have received low impressions below the Impression Threshold of " + 
-                  impressionThreshold + " impressions:\n\n"
-  };
-  
-  function main() {
+    emailMessage: function() {
+        return "The following campaigns have received low impressions below the Impression Threshold of " + 
+               this.impressionThreshold + " impressions:\n\n";
+    }()
+};
+
+function main() {
     // Select Campaigns
     let campaignIterator = AdsApp.campaigns().get();
-  
+
     // Iterate over campaigns to search for impressions below set threshold
     while (campaignIterator.hasNext()) {
-      let campaign = campaignIterator.next();
-      let stats = campaign.getStatsFor('TODAY');
-      let impressions = stats.getImpressions();
-  
-      if (impressions <= config.impressionThreshold) {
-        config.emailMessage += 'Campaign: ' + campaign.getName() + ' - Impressions: ' + impressions + '\n';
-      }
+        let campaign = campaignIterator.next();
+        let stats = campaign.getStatsFor('TODAY');
+        let impressions = stats.getImpressions();
+
+        if (impressions <= config.impressionThreshold) {
+            config.emailMessage += 'Campaign: ' + campaign.getName() + ' - Impressions: ' + impressions + '\n';
+        }
     }
-  
+
     // Send the email alert
     if (config.emailMessage.length > 'The following campaigns have received low impressions below the Impression Threshold of ' + 
                                     config.impressionThreshold + ' impressions:\n\n'.length) {
-      MailApp.sendEmail(config.emailAddresses.split(','), config.emailSubject, config.emailMessage);
-      Logger.log('Sent email alert to ' + config.emailAddresses);
+        MailApp.sendEmail(config.emailAddresses.split(','), config.emailSubject, config.emailMessage);
+        Logger.log('Sent email alert to ' + config.emailAddresses);
     } else {
-      Logger.log('No campaigns with low impressions found.');
+        Logger.log('No campaigns with low impressions found.');
     }
-  }
-  
+}
